745225e7ce4802c30994f760b22b59afe51a1dee
Arseny Egorov <egoroff-ars@yandex.ru>
2021-03-27 22:29:07 +0300

    Английские ключи для результата

--- backend/app/dependencies/filters.py
+++ backend/app/dependencies/filters.py
@@ -14,6 +14,7 @@ def get_df():
 def tours_info():
     with open("data/tours_info.json") as f_in:
         tours_info_ = json.load(f_in)
+        tours_info_ = json.loads(tours_info_)
     return tours_info_
 
 
