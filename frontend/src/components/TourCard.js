6331fc040958daef23118683cf5db15f2550fb9c
templar820 <ttemplar820@gmail.com>
2021-03-27 17:55:57 +0300

    карточка почти готова

--- frontend/src/components/TourCard.js
+++ frontend/src/components/TourCard.js
@@ -11,12 +11,12 @@ export default class TourCard extends React.Component{
         console.log(this.props.food_type)
         return(
         <Card className={"flex-row mb-4"}>
-            <Card.Img  variant="left" src="holder.js/100px180" width={"200px"} height={"200px"}/>
-            <Card.Body className={"p-4"}>
-                <section>
-                    <header>
-                        <div className={"d-flex flex-row"}>
-                            <Card.Title className={"cardLabel"}>{this.props.name}</Card.Title>
+            <Card.Img className={"p-4"} variant="left" src="https://lh3.googleusercontent.com/proxy/IuiOcqpm_6jgFp-beOcmqfwaLrbsT4D7n3RvN6J2aWcLkD6vqQgMd964mQQCewRgi1bn0TNGALTDBLF_exhmqS6LeSijKSxR6mmIJDwXG9u9og"/>
+            <Card.Body className={"pl-0"}>
+                <section className={"d-flex flex-row justify-content-between"}>
+                    <div className={"d-flex flex-column"}>
+                        <div className={"d-flex flex-row flex-wrap"}>
+                            <Card.Title className={"cardLabel mb-0"}>{this.props.name}</Card.Title>
                             <ReactStars
                               edit={false}
                               count={5}
@@ -25,19 +25,18 @@ export default class TourCard extends React.Component{
                               color2={'#ffd700'}
                             />
                         </div>
-                    </header>
-                    <main className={"d-flex flex-row justify-content-between"}>
-                        <div className="descriptionCard d-flex flex-column">
-                            <><FastfoodIcon style={{color: "#333"}}/>{this.props.food_type}</>
-                            <><FastfoodIcon style={{color: "#333"}}/>{this.props.food_type}</>
-                            <><FastfoodIcon style={{color: "#333"}}/>{this.props.food_type}</>
-                            <><FastfoodIcon style={{color: "#333"}}/>{this.props.food_type}</>
-                            <><FastfoodIcon style={{color: "#333"}}/>{this.props.food_type}</>
+                        <div className={"d-flex flex-row justify-content-between"}>
+                            <div className="descriptionCard d-flex flex-column">
+                                <span>Еда: {this.props.food_type}</span>
+                                <span>Еда: {this.props.food_type}</span>
+                                <span>Еда: {this.props.food_type}</span>
+                            </div>
                         </div>
-                        <div className="activeCard d-flex flex-column justify-content-end">
-                            <Button variant="success">Заказать тур</Button>
-                        </div>
-                    </main>
+                    </div>
+                    <div className="activeCard d-flex flex-column">
+                        <span className={"tourPrice mb-4 text-center"}>10 000 ₽</span>
+                        <Button variant="success">Заказать тур</Button>
+                    </div>
                 </section>
             </Card.Body>
         </Card>
