e8f6f9a623ec52f4d37593930626da77b303dd66
Arseny Egorov <egoroff-ars@yandex.ru>
2021-03-27 13:21:50 +0300

    Ручка на country и туры по country

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,40 @@
-from fastapi import FastAPI
+from functools import lru_cache
+import json
+import pandas as pd
+from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/")
-async def index():
-    return [{"Тур": "тур1"}, {"Тур": "тур2"}, {"Тур": "тур3"}]
+@lru_cache()
+def get_df():
+    df = pd.read_csv("data/data-full.csv")
+    return df
+
+
+@app.get("/countries")
+async def countries(df=Depends(get_df)):
+    unique_countries = list(df["Страна тура"].dropna().unique())
+    return unique_countries
+
+
+@app.get("/tours")
+async def tours(country: str, df=Depends(get_df)):
+    tours_ = json.loads(
+        df[df["Страна тура"] == country][
+            ["Наименование тура", "Звездность", "Тип питания"]
+        ]
+        .drop_duplicates()
+        .dropna()
+        .head(10)
+        .to_json(force_ascii=False, orient="records")
+    )
+    return tours_
